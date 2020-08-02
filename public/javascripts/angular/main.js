57b6fd7e26f52883ad5789bf3c63c909534e28e6
shivakomat <skomatreddy@hearsaycorp.com>
2020-08-02 13:57:02 -0700

    Adding a sample directive for new project modal and for modals in general to follow.

--- public/javascripts/angular/main.js
+++ public/javascripts/angular/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var app =
-    angular.module('wellPlanner', ['auth0', 'angular-storage', 'angular-jwt', 'ngMaterial', 'ui.router']);
+    angular.module('wellPlanner', ['auth0', 'angular-storage', 'angular-jwt', 'ngMaterial', 'ui.router', 'ui.bootstrap']);
 
 app.config(function ($provide, authProvider, $urlRouterProvider, $httpProvider, jwtInterceptorProvider) {
         $urlRouterProvider.otherwise('/');
