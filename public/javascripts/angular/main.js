b0b8ea995dd68bfe7cea35cceed1a5c0a7e7fafa
shivakomat <skomatreddy@hearsaycorp.com>
2020-08-20 17:01:34 -0700

    removing ui bootstrap cdnjs reference library

--- public/javascripts/angular/main.js
+++ public/javascripts/angular/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var app =
-    angular.module('wellPlanner', ['auth0', 'angular-storage', 'angular-jwt', 'ngMaterial', 'ui.router', 'ui.bootstrap']);
+    angular.module('wellPlanner', ['auth0', 'angular-storage', 'angular-jwt', 'ngMaterial', 'ui.router']);
 
 app.config(function ($provide, authProvider, $urlRouterProvider, $httpProvider, jwtInterceptorProvider) {
         $urlRouterProvider.otherwise('/');
