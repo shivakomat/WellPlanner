85a928e7aea19786ed87a8de215eb49f0bd3d352
shivakomat <skomatreddy@hearsaycorp.com>
2020-08-22 11:49:13 -0700

    adding sc delegate provider configuraiton

--- public/javascripts/angular/main.js
+++ public/javascripts/angular/main.js
@@ -3,6 +3,13 @@
 var app =
     angular.module('wellPlanner', ['auth0', 'angular-storage', 'angular-jwt', 'ngMaterial', 'ui.router']);
 
-app.config(function ($provide, authProvider, $urlRouterProvider, $httpProvider, jwtInterceptorProvider) {
+app.config(function ($provide, authProvider, $urlRouterProvider, $httpProvider, jwtInterceptorProvider, $sceDelegateProvider) {
         $urlRouterProvider.otherwise('/');
+
+        $sceDelegateProvider.resourceUrlWhitelist([
+                // Allow same origin resource loads.
+                'self',
+                // Allow loading from our assets domain. **.
+                'http://well-wedding-planner.herokuapp.com/assets/**'
+        ]);
 });
