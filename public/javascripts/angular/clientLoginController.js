dbb7e4ad8ff4bbdb479008b98ac501056a831bf7
shivakomat <skomatreddy@hearsaycorp.com>
2021-12-11 18:21:22 -0800

    Bug fix re direct url fixed

--- public/javascripts/angular/clientLoginController.js
+++ public/javascripts/angular/clientLoginController.js
@@ -6,8 +6,7 @@ app.controller('clientLoginController', function($http, $window, ClientAccessFac
         var loginInfo = clientLoginController.loginFormData;
         ClientAccessFactory.loginClient(loginInfo, function mySuccess(response) {
             clientLoginController.loginFormData = {};
-            var targetUrl =  "https://" + $window.location.host + "/pages/client-portal/" + response.data.data.businessId +  "/projects/" + response.data.data.projectId;
-            $window.location.href = targetUrl;
+            $window.location.href = "https://" + $window.location.host + "/pages/client-portal/" + response.data.data.businessId + "/projects/" + response.data.data.projectId;
         }, function myError(errMsg) {
             clientLoginController.loginFormData = {};
             console.log(errMsg);
