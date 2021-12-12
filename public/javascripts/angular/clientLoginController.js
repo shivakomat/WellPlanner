69c436d1dec5b5215a2ae4d55b690a41e85e6859
shivakomat <skomatreddy@hearsaycorp.com>
2021-12-11 18:04:43 -0800

    Bug fix login button change to non submit type

--- public/javascripts/angular/clientLoginController.js
+++ public/javascripts/angular/clientLoginController.js
@@ -7,8 +7,9 @@ app.controller('clientLoginController', function($http, $window, ClientAccessFac
         ClientAccessFactory.loginClient(loginInfo, function mySuccess(response) {
             clientLoginController.loginFormData = {};
             console.log(response.data.data);
-            $window.location.href =
-                "https://" + $window.location.host + "/pages/client-portal/" + response.data.data.businessId +  "/projects/" + response.data.data.projectId
+            var target_url =  "https://" + $window.location.host + "/pages/" + response.data.data.business_id +  "/projects/" + response.data.data.project_id + "/projectSettings";
+            console.log(target_url);
+            $window.location.href = target_url;
         }, function myError(errMsg) {
             clientLoginController.loginFormData = {};
             console.log(errMsg);
