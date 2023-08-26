55fb8110697e8f02b2a361161145a61d1b866cbf
shivakomat <skomatreddy@hearsaycorp.com>
2023-08-25 20:57:34 -0700

    putting back is_deleted

--- app/model/dataModels/Project.scala
+++ app/model/dataModels/Project.scala
@@ -11,4 +11,5 @@ case class Project(id: Option[Int] = None,
                    event_date: Int,
                    business_id: Int,
                    modified_date: Int,
-                   created_date: Int)
+                   created_date: Int,
+                   is_deleted: Boolean = false)
